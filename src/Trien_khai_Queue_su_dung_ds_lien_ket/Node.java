fb4871ed00d435812e95a5831f151bd47a081844
dunghojava <dungho21397@gmail.com>
2022-04-04 10:02:09 +0700

    Trien khai queue su dung lien ket

---
+++ src/Trien_khai_Queue_su_dung_ds_lien_ket/Node.java
@@ -0,0 +1,11 @@
+package Trien_khai_Queue_su_dung_ds_lien_ket;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
